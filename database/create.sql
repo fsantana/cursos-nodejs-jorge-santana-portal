30e8a29d5cd40e7bf099ade1f0947432c7006686
Fernando Santana <fernandosantanajr@gmail.com>
2021-08-10 23:42:23 -0300

    adding fields to noticias table

--- database/create.sql
+++ database/create.sql
@@ -4,6 +4,9 @@ CREATE TABLE `noticias` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `titulo` varchar(100) DEFAULT NULL,
   `noticia` text,
+  `resumo` varchar(100) DEFAULT NULL,
+  `autor` varchar(30) DEFAULT NULL,
+  `data_noticia` date DEFAULT NULL,
   `data_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4
