1219b53f47dc62636bccaef9bd8e257df1e9403c
Fernando Santana <fernandosantanajr@gmail.com>
2021-08-14 02:28:46 -0300

    use arrow funciontions on routes

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,9 +1,9 @@
-module.exports = function(application){
-    application.get('/formulario_inclusao_noticia', function(req,res){
+module.exports = (application) => {
+    application.get('/formulario_inclusao_noticia', (req,res) => {
         application.app.controllers.admin.formulario_inclusao_noticia(application, req, res);
     });
 
-    application.post('/noticias/salvar', function(req,res){
+    application.post('/noticias/salvar', (req,res) => {
         application.app.controllers.admin.noticias_salvar(application, req, res)
     });
 }
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,12 +1,12 @@
-module.exports = function(application){
+module.exports = (application) => {
 
-  application.get('/noticias', function(req,res){
+  application.get('/noticias', (req,res) => {
 
       application.app.controllers.noticias.noticias(application, req, res);
 
   });
 
-  application.get('/noticia', function(req,res){
+  application.get('/noticias/:id', (req,res) => {
 
       application.app.controllers.noticias.noticia(application, req, res);
 
